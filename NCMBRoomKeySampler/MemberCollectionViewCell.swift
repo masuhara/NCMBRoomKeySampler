822fea07c073185afab202d162d82ce126b44f3a
masuhara <info@masuhara.net>
2019-01-06 09:19:52 +0900

    Add member custom collectionViewCell

---
+++ NCMBRoomKeySampler/MemberCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MemberCollectionViewCell.swift
+//  NCMBRoomKeySampler
+//
+//  Created by Masuhara on 2019/01/06.
+//  Copyright © 2019 Ylab, Inc. All rights reserved.
+//
+
+import UIKit
+
+class MemberCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
